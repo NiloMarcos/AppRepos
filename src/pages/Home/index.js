4484505a67066657b77899c78b16f59f5a5b7ac1
Nilo Marcos <nilo.n2m15@gmail.com>
2021-08-22 15:32:16 -0300

    implement of React Hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import SearchIcon from '../../assets/pesquisa.svg';
 import EmphasisIcon from '../../assets/destaque.svg';
 import SaveIcon from '../../assets/salvo_azul.svg';
@@ -6,10 +6,15 @@ import ArrowRight from '../../assets/seta_direita_branco.svg';
 import { Container, ContainerSearch, Input, ContainerInformations, Title, SubTitle, ContainerRepos, ButtonNextSceen, ButtonNextSceenText} from './styles';
 
 export default function Home({navigation}) {
+  const [newRepo, setNewRepo] = useState('');
+  const [repoApi, setRepoApi] = useState([]);
+
+  console.log(newRepo);
+
   return (
     <Container>
       <ContainerSearch>
-        <Input placeholder="Procurar organizações" maxLength={35}/>
+        <Input placeholder="Procurar organizações..." maxLength={35} value={newRepo} onChangeText={(text) => setNewRepo(text)} />
         <SearchIcon  width={120} height={30} top={62} left={-70} />
       </ContainerSearch>
 
